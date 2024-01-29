59421edc02493ca995305b261b88c21a7d19b663
tia <tiasolanki@gmail.com>
2024-01-29 18:33:57 +0100

    imit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='open-source-decTD',
+    version='1.0',
+    author='tiasolanki',
+    license='MIT',
+    long_description=open('README.md').read()
+)
